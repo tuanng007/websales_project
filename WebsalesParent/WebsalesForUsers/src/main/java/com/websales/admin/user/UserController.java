f4475cb5dd4e5ff84bc6a370286a5e1380f0c7f8
tuanng007 <minh000xyz@gmail.com>
2024-11-16 22:27:29 +0700

    update sort function

--- WebsalesParent/WebsalesForUsers/src/main/java/com/websales/admin/user/UserController.java
+++ WebsalesParent/WebsalesForUsers/src/main/java/com/websales/admin/user/UserController.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
@@ -29,30 +30,34 @@ public class UserController {
 	
 	@GetMapping("/users")
 	public String listFirstPage(Model model) { 
-		return listByPage(1, model);
+		return listByPage(1, model,"firstName", "asc");
 	}
 	
 	
 	@GetMapping("/users/page/{pageNum}")
-	public String listByPage(@PathVariable(name="pageNum") int pageNum, Model model) {
-		Page<User> page = userService.listByPage(pageNum);
+	public String listByPage(@PathVariable(name="pageNum") int pageNum, Model model
+			, @Param("sortField") String sortField, @Param("sortDir") String sortDir) {
+		Page<User> page = userService.listByPage(pageNum, sortField, sortDir);
 		List<User> listUsers = page.getContent();
 		
-//		System.out.println("Pagenum: " + pageNum);
-//		System.out.println("Total element: " + page.getTotalElements());
-//		System.out.println("Total page: " + page.getTotalPages());
-		
+		System.out.println("Sort field: " + sortField);
+		System.out.println("sort order: " + sortDir);
 		long startCount = (pageNum - 1) * UserService.USERS_PER_PAGE + 1;
 		long endCount = startCount + UserService.USERS_PER_PAGE - 1;
 		if(endCount > page.getTotalElements()) { 
 			endCount = page.getTotalElements();
 		}
 		
+		String reSort = sortDir.equals("asc") ? "desc" : "asc"; 
+		
 		model.addAttribute("currentPage", pageNum);
 		model.addAttribute("totalPages", page.getTotalPages());
 		model.addAttribute("startCount", startCount);
 		model.addAttribute("endCount", endCount);
 		model.addAttribute("totalItems", page.getTotalElements());
+		model.addAttribute("sortField", sortField);
+		model.addAttribute("sortDir", sortDir);
+		model.addAttribute("reSort", reSort);
 		model.addAttribute("listUsers", listUsers);
 		
 		return "users";
