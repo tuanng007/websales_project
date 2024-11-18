a914df4dba175d05a0518583112ca5aa98a398f3
tuanng007 <minh000xyz@gmail.com>
2024-11-18 23:28:17 +0700

    new login form for user, authentication

--- WebsalesParent/WebsalesForUsers/src/main/java/com/websales/admin/security/SecurityConfiguration.java
+++ WebsalesParent/WebsalesForUsers/src/main/java/com/websales/admin/security/SecurityConfiguration.java
@@ -2,27 +2,68 @@ package com.websales.admin.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 public class SecurityConfiguration {
-	
-	@Bean
-	public PasswordEncoder PasswordEncoder() { 
-		return new BCryptPasswordEncoder();
-	}
-	
-	@Bean
-	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception { 
-		http
-        .authorizeHttpRequests((authz) -> authz
-            .anyRequest().permitAll()
-        )
-        .httpBasic(withDefaults());
-    return http.build();
-	}
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return new WebsalesUserDetailsService();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider auProvider = new DaoAuthenticationProvider();
+        auProvider.setUserDetailsService(userDetailsService());
+        auProvider.setPasswordEncoder(passwordEncoder());
+        return auProvider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .authorizeHttpRequests(authz -> authz
+                .requestMatchers("/login").permitAll()
+                .anyRequest().authenticated()
+            )
+            .formLogin(form -> form
+                .loginPage("/login").usernameParameter("email").permitAll()
+            )
+            .httpBasic(withDefaults());
+        return http.build();
+    }
+    
+//    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//        http
+//            .authorizeHttpRequests(authz -> authz
+//           
+//                .anyRequest().permitAll()
+//            )
+////            .formLogin(form -> form
+////                .loginPage("/login").usernameParameter("email").permitAll()
+////            )
+//            .httpBasic(withDefaults());
+//        return http.build();
+//    }
 }
---
+++ WebsalesParent/WebsalesForUsers/src/main/java/com/websales/admin/security/WebSalesUserDetails.java
@@ -0,0 +1,73 @@
+package com.websales.admin.security;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.websales.common.entity.Role;
+import com.websales.common.entity.User;
+
+public class WebSalesUserDetails implements UserDetails {
+	
+	private User user;
+	
+	public WebSalesUserDetails(User user) { 
+		this.user = user;
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		Set<Role> roles = user.getRoles();
+		
+		List<SimpleGrantedAuthority> authorities = new ArrayList<>();
+		
+		for(Role role : roles) {
+			authorities.add(new SimpleGrantedAuthority(role.getName()));
+		}
+		
+		return authorities;
+	}
+
+	@Override
+	public String getPassword() {
+		
+		return user.getPassword();
+	}
+
+	@Override
+	public String getUsername() {
+		
+		return user.getEmail();
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return user.isEnabled();
+	}
+	
+	
+	
+	
+}
